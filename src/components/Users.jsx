8a5622c154fe9935b538f28f9da643e60d8eca7c
Rocky3112 <alomgirhossainrocky@gmail.com>
2023-05-08 01:53:04 +0600

    delete operation successfully worked

--- src/components/Users.jsx
+++ src/components/Users.jsx
@@ -1,16 +1,34 @@
 /* eslint-disable no-unused-vars */
-import React from 'react';
+import React, { useState } from 'react';
 import { useLoaderData } from 'react-router-dom';
 
 const Users = () => {
-    const user = useLoaderData()
-    console.log(user);
+    const loadedUser = useLoaderData()
+    // console.log(user);
+    const [users, setUsers] = useState(loadedUser);
+
+    const handleDelete = _id =>{
+        console.log(_id);
+        fetch(`http://localhost:5000/users/${_id}`,{
+            method:'DELETE'
+        })
+        .then(res =>res.json())
+        .then(data=>{
+            console.log(data);
+            if(data.deletedCount>0){
+                alert('deleted successfully')
+                const remaining = users.filter(user =>user._id !==_id);
+                setUsers(remaining)
+            }
+        })
+
+    }
     return (
         <div>
-            <h2>{user.length}</h2>
+            <h2>{users.length}</h2>
             <div>
                 {
-                    user.map(user => <p key ={user._id}>{user.name} :{user.email}</p>
+                    users.map(user => <p key ={user._id}>{user.name} :{user.email} <button onClick={()=> handleDelete(user._id)}>X</button> </p>
                     )
                 }
             </div>
